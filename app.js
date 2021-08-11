50763fbd33801dfc52172d6b0fc93916a53b5d46
Naima Hassan <naima.hassan434@gmail.com>
2021-08-11 20:27:10 +0100

    Add new notes

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 // Write Your Javascript Code Here
 class App {
   constructor() {
-    console.log("Code is running");
+    // console.log("Code is running");
+
+    //State control
+
+    this.notes = [];
+
+    //html elements
     this.form = document.querySelector("#form");
     this.noteTitle = document.querySelector("#note-title");
+    this.noteText = document.querySelector("#note-text");
     this.formButtons = document.querySelector("#form-buttons");
 
+    //METHOD
     this.addEventListeners();
   }
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
+    });
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = this.noteTitle.value;
+      const text = this.noteText.value;
+      const hasNote = title || text;
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
     });
   }
   handleFormClick(event) {
@@ -32,6 +49,20 @@ class App {
     this.form.classList.remove(".form-open");
     this.noteTitle.style.display = "none";
     this.formButtons.style.display = "none";
+    this.noteTitle.value = "";
+    this.noteText.value = "";
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    };
+    this.notes = [...this.notes, newNote];
+    // console.log(this.notes);
+    this.closeForm();
   }
 }
 new App();
