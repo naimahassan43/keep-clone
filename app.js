df6f13efc2cc553a8f0c5ebbed2cdecd6b94a67c
Naima Hassan <naima.hassan434@gmail.com>
2021-08-11 19:50:09 +0100

    Expand and Shrink the form when clicked

--- app.js
+++ app.js
@@ -2,6 +2,36 @@
 class App {
   constructor() {
     console.log("Code is running");
+    this.form = document.querySelector("#form");
+    this.noteTitle = document.querySelector("#note-title");
+    this.formButtons = document.querySelector("#form-buttons");
+
+    this.addEventListeners();
+  }
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+  handleFormClick(event) {
+    const isFormClicked = this.form.contains(event.target);
+    if (isFormClicked) {
+      //expand the form
+      this.openForm();
+    } else {
+      //shrink the form
+      this.closeForm();
+    }
+  }
+  openForm() {
+    this.form.classList.add(".form-open");
+    this.noteTitle.style.display = "block";
+    this.formButtons.style.display = "block";
+  }
+  closeForm() {
+    this.form.classList.remove(".form-open");
+    this.noteTitle.style.display = "none";
+    this.formButtons.style.display = "none";
   }
 }
 new App();
